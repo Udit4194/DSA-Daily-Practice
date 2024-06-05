92ba77ef4674936569752ba6edd24f2e559d425b
Udit Kumar <167570748+Udit4194@users.noreply.github.com>
2024-06-06 02:39:08 +0530

    Create Sort function.cpp

---
+++ Udit/Array/Sort function.cpp
@@ -0,0 +1,10 @@
+//Given a random set of numbers, Print them in ascending sorted order.
+
+class Solution{
+    public:
+    vector<int> sortArr(vector<int>arr, int n){
+    //complete the function here
+  sort(arr.begin(), arr.end());
+  return arr;
+    }
+};
